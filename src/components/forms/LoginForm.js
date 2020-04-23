60ee5e757315f86edc34168e5c1b8011046dd259
NANAKAY <brownjay1997@.com>
2020-04-23 04:43:21 +0000

    Sign udpated

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 
-import {Form,Container,Modal,Button} from 'react-bootstrap';
+import {Form,Container,Modal,Button,Row,Col} from 'react-bootstrap';
 
 function LoginForm (props){
     const [validated, setValidated] = useState(false);
@@ -22,11 +22,14 @@ function LoginForm (props){
       centered
       
     >
+    <Modal.Header closeButton>
+          <Modal.Title  style={{marginLeft:'150px'}} >Flight-Booky</Modal.Title>
+        </Modal.Header>
         <Container style={{width:'400px',height:'600px'}} fluid>
-        <Form noValidate validated={validated} onSubmit={handleSubmit}style={{justifyContent:'center',alignItems:'center',marginTop:'60px'}}>
+        <Form variant="primary" noValidate validated={validated} onSubmit={handleSubmit}style={{justifyContent:'center',alignItems:'center',marginTop:'100px'}}>
   <Form.Group controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
-    <Form.Control required type="email" placeholder="Enter email" style={{width:'400px'}} />
+    <Form.Control required type="email" placeholder="Enter email" style={{width:'400px'}} size="lg" />
     <Form.Text className="text-muted">
       Example: example@example.com
     </Form.Text>
@@ -37,7 +40,7 @@ function LoginForm (props){
   
   <Form.Group controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control required type="password" placeholder="Password" style={{width:'400px'}}/>
+    <Form.Control required type="password" placeholder="Password" style={{width:'400px'}} size="lg"/>
     <Form.Control.Feedback type="invalid">
     Please enter a correct password
   </Form.Control.Feedback>
@@ -45,11 +48,19 @@ function LoginForm (props){
   <Form.Group controlId="formBasicCheckbox">
     <Form.Check type="checkbox" label="Remember Me" />
   </Form.Group>
-  <Button variant="primary" type="submit">
+  <Button variant="primary" type="submit" size="lg" block>
     Sign In
   </Button>
   </Form>
         </Container>
+        <Modal.Footer>
+        <Row>
+        <Col style={{marginRight:'100px'}}>Do you have an account?</Col>
+        <Col>
+        <Button>Create account</Button>
+        </Col>
+        </Row>
+        </Modal.Footer>
         </Modal>
     )
   }
