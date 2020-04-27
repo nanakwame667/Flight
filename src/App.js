6b4c515481bbb844bc1a75c434a1ab60e39bd0c9
Code-Chingy <chingy.debillz@gmail.com>
2020-04-27 15:59:36 +0000

    added admin routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
 
-import {Route,Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 
 import NavBar from '../src/components/forms/home/NavBar';
 
 import FlightsPage from './components/pages/FlightsPage';
 import HomePage from './components/pages/HomePage';
 import SignUpPage from './components/pages/SignUpPage';
+import AdminLoginPage from './screens/login';
+import AdminRegisterPage from './screens/signup';
+import AdminDashboardPage from './screens/dashboard';
 
 
 
 function App() {
   return (
     <div>
-    <NavBar/>
-    <Switch>
-   <Route path="/" exact component={HomePage}/>
-   <Route path="/SignUp" exact component={SignUpPage}/>
-   <Route path="/flights" exact component={FlightsPage}/>
-   </Switch>
+      <NavBar/>
+      <Switch>
+        <Route path="/" exact component={HomePage}/>
+        <Route path="/SignUp" exact component={SignUpPage}/>
+        <Route path="/flights" exact component={FlightsPage}/>
+          <Route path="/admin/signup" exact component={AdminRegisterPage}/> 
+          <Route path="/admin/dashboard" exact component={AdminDashboardPage}/>
+          <Route path="/admin/login" exact component={AdminLoginPage}/> 
+          <Redirect from="/admin" to='/admin/login'/> 
+    </Switch>
     </div>
   );
 }
